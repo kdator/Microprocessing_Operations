f00dd762e9dd137a88079b7ec5b414badebaaa82
kdator <colpakov.dmitry@gmail.com>
2020-12-07 21:25:37 +0300

    lab3: added.

---
+++ lab3/data_memory.v
@@ -0,0 +1,28 @@
+/*
+	\brief Модуль памяти.
+	\date 22-11-2020
+	\author kolpakov dmitrii
+*/
+
+module data_memory (
+		input		[31:0] 	addr_i, // считываемый адрес.
+		input 	[31:0] 	wd_i,   // данные для записи.
+		input 		    	clk_i,  // тактовый сигнал.
+		input 	       	we_i,   // write enable сигнал.
+		output wire	[31:0] rd_o   // данные, которые выдаются с data memory.
+);
+
+	reg [31:0] MEMORY [0:63]; // типо память.
+	initial $readmemb ("C:\\Users\\User\\Desktop\\test.txt", MEMORY); // считываем тестовые данные в память.
+
+	assign rd_o = (24'h660000 == addr_i[31:8]) ?  MEMORY[addr_i[7:2]] : 32'd0;
+
+	always @ (posedge clk_i)
+		begin
+			if (we_i)
+				begin
+					MEMORY[addr_i[7:2]] <= wd_i; // записываем слово, поэтому последние два бита откидываем.
+				end
+		end
+
+endmodule
---
+++ lab3/register_file.v
@@ -0,0 +1,42 @@
+/*
+	\brief Модуль регистрового файла.
+	\date 22-11-2020
+	\author kolpakov dmitrii
+*/
+
+ module register_file (
+		input    [4:0] 	addr1_i,  // адрес первого операнда.
+		input 	[4:0] 	addr2_i,  // адрес второго операнда.
+		input 	[4:0] 	wd_addr_i,  // адрес для записи результата.
+		input 	[31:0]	wd_i,	    // порт, куда запишутся данные.
+		input 				clk_i,    // тактовый сигнал CLK.
+		input 				we_i,     // write enable сигнал.
+		input					reset,    // reset сигнал.
+		output 	[31:0]   rd1_o,    // данные, считанные с первого адресса.
+		output	[31:0]   rd2_o	    // данные, считаннве со второго адресса.
+	);
+
+	integer i; // переменная для сброса данных в цикле.
+	reg [31:0] RAM [0:31]; // 32 регистра по 32 бита каждый.
+
+	// считываем данные из RAM на выход.
+	assign rd1_o = (addr1_i != 32'd0) ?  RAM[addr1_i] : 32'd0;
+	assign rd2_o = (addr2_i != 32'd0) ?  RAM[addr2_i] : 32'd0;
+
+	always @ (posedge clk_i)
+		begin
+			if (we_i)
+				begin
+					if (wd_addr_i != 5'd0) // убеждаемся, что не пишем в константный ноль.
+						RAM[wd_addr_i] <= wd_i;
+				end
+			if (reset)
+				begin
+					for (i = 0; i < 32; i = i + 1)
+						begin
+							RAM[i] <= 32'd0;
+						end
+				end
+		end
+		
+endmodule
