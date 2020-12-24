3cfc17d7776a010a668ec03b04478d81940e3257
kdator <colpakov.dmitry@gmail.com>
2020-12-24 11:34:06 +0300

    lab3: fixed the bug with byte offset.

--- lab3/src/data_memory.v
+++ lab3/src/data_memory.v
@@ -9,20 +9,37 @@ module data_memory (
 		input 	[31:0] 	wd_i,   // данные для записи.
 		input 		    	clk_i,  // тактовый сигнал.
 		input 	       	we_i,   // write enable сигнал.
-		output wire	[31:0] rd_o   // инструкция из памяти.
+		output reg	[31:0] rd_o   // инструкция из памяти.
 );
 
-	reg [31:0] MEMORY [0:63]; // типо память.
+	reg [7:0] MEMORY [0:254]; // типо память.
+	reg [31:0] instr_begin;   // начало инструкции.
 	initial $readmemb ("C:\\Users\\User\\Desktop\\test.txt", MEMORY); // считываем тестовые данные в память.
-
-	assign rd_o = (24'h660000 == addr_i[31:8]) ?  MEMORY[addr_i[7:2]] : 32'd0; // считываем машинное слово целиком.
 
 	always @ (posedge clk_i)
 		begin
+			if (addr_i >= 32'h66000000 && addr_i <= 32'h660000FC)
+				begin
+					instr_begin <= (addr_i & (8'hff-3));
+					rd_o[31:24] <= MEMORY[instr_begin + 3];
+					rd_o[23:16] <= MEMORY[instr_begin + 2];
+					rd_o[15:8] <= MEMORY[instr_begin + 1];
+					rd_o[7:0] <= MEMORY[instr_begin];
 			if (we_i)
 				begin
-					MEMORY[addr_i[7:2]] <= wd_i; // записываем слово, поэтому последние два бита откидываем.
+					MEMORY[instr_begin] <= wd_i[7:0];
+					MEMORY[instr_begin + 1] <= wd_i[15:8];
+					MEMORY[instr_begin + 2] <= wd_i[23:16];
+					MEMORY[instr_begin + 3] <= wd_i[31:24];
 				end
 		end
-
+		else
+			if (!we_i)
+				begin
+					rd_o[31:24] <= 0;
+					rd_o[23:16] <= 0;
+					rd_o[15:8]  <= 0;
+					rd_o[7:0]   <= 0;
+				end
+		end
 endmodule
