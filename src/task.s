d6bfadbb55dcd387a5234e0223dfade618d0b104
Dmitrii <48220756+kdator@users.noreply.github.com>
2021-01-10 02:49:12 +0300

    Update task.s
    project: the marking moved out.

--- src/task.s
+++ src/task.s
@@ -15,24 +15,24 @@ return:
 		jal Exit            #                 || exit programm
 
 factorial:
-		addi x12, x8, 0                       #  number_to_return = number;
-        sub x13, x8, x10                      #  int i = number - i;
-		begin_first_loop:
-        		beq x13, x0, end_first_loop   #  if (i == 0) jump to the end of the first cycle.
-                addi x14, x0, 0				  #  sum = 0;
-                addi x15, x13, 0			  #  int j = i;
+	addi x12, x8, 0                       ||  number_to_return = number;
+        sub x13, x8, x10                      ||  int i = number - i;
+	begin_first_loop:
+         	beq x13, x0, end_first_loop   ||  if (i == 0) jump to the end of the first cycle.
+                addi x14, x0, 0		      ||  sum = 0;
+                addi x15, x13, 0	      ||  int j = i;
         begin_second_loop:
-        		beq x15, x0, end_second_loop  #  if (j == 0) jump to the end of the second cycle.
-                add x14, x14, x12 			  #  sum += number_to_return;
-                sub x15, x15, x10             #  j--;
-                j begin_second_loop			  #  back to the begin of the second cycle.
+        	beq x15, x0, end_second_loop  ||  if (j == 0) jump to the end of the second cycle.
+                add x14, x14, x12 	      ||  sum += number_to_return;
+                sub x15, x15, x10             ||  j--;
+                j begin_second_loop	      ||  back to the begin of the second cycle.
         end_second_loop:
-        		addi x12, x14, 0              #  number_to_return = sum;
-                sub x13, x13, x10             #  i--;
-                j begin_first_loop            #  back to the begin of the first cycle.
+        	addi x12, x14, 0              ||  number_to_return = sum;
+                sub x13, x13, x10             ||  i--;
+                j begin_first_loop            ||  back to the begin of the first cycle.
         end_first_loop:
-        		addi x9, x12, 0               #  return number_to_return;
-				ret                     #                   || Returning to main func
+        	addi x9, x12, 0               ||  return number_to_return;
+		ret                           ||
 
 Exit:
 	nop
